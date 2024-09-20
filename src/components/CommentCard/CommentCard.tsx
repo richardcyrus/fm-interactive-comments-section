a51cdc91e2cc1236f942c4e8aacd05b85cde336d
Richard Cyrus <richard.cyrus@rcyrus.com>
2024-09-19 22:16:57 -0400

    complete basic layouts for comment card

--- src/components/CommentCard/CommentCard.tsx
+++ src/components/CommentCard/CommentCard.tsx
@@ -1,53 +1,89 @@
 import Avatar from '@/components/Avatar/Avatar'
+import { useAuth } from '@/hooks/useAuth'
 import type { Comment } from '@/models/db'
 import { getUser } from '@/models/users'
-import { formatDistanceToNow } from 'date-fns'
+import { formatDistanceToNowStrict } from 'date-fns'
 import { useLiveQuery } from 'dexie-react-hooks'
 
+import IconDelete from '@/assets/icons/icon-delete.svg'
+import IconEdit from '@/assets/icons/icon-edit.svg'
 import IconMinus from '@/assets/icons/icon-minus.svg'
 import IconPlus from '@/assets/icons/icon-plus.svg'
 import IconReply from '@/assets/icons/icon-reply.svg'
 
 function CommentCard({ comment }: { comment: Comment }) {
   const user = useLiveQuery(() => getUser(comment.user), [comment.user])
+  const { currentUser } = useAuth()
 
   return (
     <div
-      className="grid grid-cols-2 grid-rows-[auto_1fr_auto] gap-4 rounded-[8px] bg-white p-4 font-normal [&:not(:first-of-type)]:mt-4"
+      className="grid max-w-[730px] grid-cols-2 grid-rows-[auto_1fr_auto] gap-4 rounded-[8px] bg-white p-4 font-normal sm:grid-cols-[auto_1fr_auto] sm:grid-rows-[auto_1fr] sm:gap-x-6 sm:p-6 [&:not(:first-of-type)]:mt-4"
       data-comment-id={comment.id}
     >
-      <div className="col-span-2 flex items-center">
+      <div className="col-span-2 flex items-center sm:col-auto sm:col-start-2 sm:row-start-1">
         <Avatar
           username={user?.username as string}
           imageUrl={user?.image.webp || ''}
         />
         <div className="ms-4 font-medium text-blue-950">{user?.username}</div>
+        {currentUser === comment.user ? (
+          <div className="ms-2 shrink-0 rounded-[2px] bg-blue-500 px-[6px] text-[13px] text-white">
+            you
+          </div>
+        ) : null}
         <div className="ms-4 leading-6">
           {comment.createdAt &&
-            formatDistanceToNow(new Date(comment.createdAt), {
+            formatDistanceToNowStrict(new Date(comment.createdAt), {
               addSuffix: true,
             })}
         </div>
       </div>
-      <div className="col-span-2 leading-6 text-gray-600">
+      <div className="col-span-2 leading-6 text-gray-600 sm:col-start-2">
         {comment.content}
       </div>
-      <div className="flex w-24 items-center justify-between rounded-[10px] bg-gray-100 px-4 py-[10px]">
-        <button type="button" className="h-[11px] w-[11px]">
+      <div className="flex h-10 w-[100px] items-center justify-between rounded-[10px] bg-gray-100 px-4 py-[10px] sm:col-start-1 sm:row-span-full sm:row-start-1 sm:h-[100px] sm:w-10 sm:flex-col">
+        <button
+          type="button"
+          className="h-[11px] w-[11px] text-blue-300 hover:text-blue-500"
+        >
           <IconPlus />
         </button>
         <span className="font-medium text-blue-500">{comment.score}</span>
-        <button type="button" className="h-[3px] w-[11px]">
+        <button
+          type="button"
+          className="h-[3px] w-[11px] text-blue-300 hover:text-blue-500"
+        >
           <IconMinus />
         </button>
       </div>
-      <button
-        className="flex items-center justify-self-end font-medium text-blue-500"
-        data-comment-id={comment.id}
-      >
-        <IconReply className="me-2 h-[13px] w-[14px]" />
-        <span>Reply</span>
-      </button>
+      {currentUser === comment.user ? (
+        <div className="flex items-center justify-self-end sm:row-start-1">
+          <button
+            className="me-4 flex items-center font-medium leading-6 text-red-600 hover:text-red-200"
+            data-comment-id={comment.id}
+          >
+            <IconDelete className="me-2 h-[14px] w-[12px]" />
+            <span>Delete</span>
+          </button>
+          <button
+            className="flex items-center font-medium leading-6 text-blue-500 hover:text-blue-300"
+            data-comment-id={comment.id}
+          >
+            <IconEdit className="me-2 h-[14px] w-[14px]" />
+            <span>Edit</span>
+          </button>
+        </div>
+      ) : (
+        <div className="flex items-center justify-self-end sm:row-start-1">
+          <button
+            className="flex items-center justify-self-end font-medium text-blue-500 hover:text-blue-300"
+            data-comment-id={comment.id}
+          >
+            <IconReply className="me-2 h-[13px] w-[14px]" />
+            <span>Reply</span>
+          </button>
+        </div>
+      )}
     </div>
   )
 }
