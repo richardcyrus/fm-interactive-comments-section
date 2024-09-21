f243bbff06a99740ce870db6ab0d42b2109d68a5
Richard Cyrus <richard.cyrus@rcyrus.com>
2024-09-21 00:01:28 -0400

    add tests for user switcher

--- test/vitest-setup.ts
+++ test/vitest-setup.ts
@@ -3,3 +3,35 @@ import 'fake-indexeddb/auto'
 import { expect } from 'vitest'
 
 expect.extend(matchers)
+window.HTMLElement.prototype.scrollIntoView = vi.fn()
+window.HTMLElement.prototype.hasPointerCapture = vi.fn()
+window.HTMLElement.prototype.releasePointerCapture = vi.fn()
+window.PointerEvent = MouseEvent as typeof window.PointerEvent
+class ResizeObserver {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  cb: any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(cb: any) {
+    this.cb = cb
+  }
+
+  // @ts-expect-error ts(6133)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+  observe(cb: any) {
+    this.cb([{ borderBoxSize: { inlineSize: 0, blockSize: 0 } }])
+  }
+  unobserve() {}
+  disconnect() {}
+}
+global.ResizeObserver = ResizeObserver
+global.DOMRect = {
+  // @ts-expect-error ts(2379)
+  fromRect: () => ({
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+  }),
+}
